5ac2e19fc8bdf83bac00c80feb4c200992b9d559
manhtai <vo@manhtai.com>
2017-11-04 22:15:33 +0700

    Parse prefix operators

--- ast/ast.go
+++ ast/ast.go
@@ -137,6 +137,31 @@ func (es *ExpressionStatement) String() string {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// PREFIX EXPRESSION
+///////////////////////////////////////////////////////////////////////////////
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // INTEGER
 ///////////////////////////////////////////////////////////////////////////////
 
