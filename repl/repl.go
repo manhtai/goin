7df8d55f79d3625a3a4b26f2cb6c8d55dcaf5e5f
manhtai <vo@manhtai.com>
2017-11-05 23:38:45 +0700

    RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -3,9 +3,10 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"github.com/manhtai/goin/lexer"
-	"github.com/manhtai/goin/token"
 	"io"
+
+	"github.com/manhtai/goin/lexer"
+	"github.com/manhtai/goin/parser"
 )
 
 // PROMPT is our prompt characters
@@ -22,8 +23,22 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
